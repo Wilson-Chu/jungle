7b1102ea4c4ba65d987c9249f23bbe0b734f3eb8
Wilson Chu <wilson1.chu@gmail.com>
2023-11-22 14:25:43 -0500

    setup sessions routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,9 @@ Rails.application.routes.draw do
 
   resources :products, only: [:index, :show]
   resources :categories, only: [:show]
-  resources :about, only: [:index]
-  resources :users, only: [:new, :create]
+  # resources :about, only: [:index]
+  # resources :users, only: [:new, :create]
+  # resources :sessions, only: [:new, :create, :destroy]
 
   resource :cart, only: [:show] do
     post   :add_item
@@ -20,6 +21,13 @@ Rails.application.routes.draw do
     resources :products, except: [:edit, :update, :show]
     resources :categories, only: [:index, :create, :new]
   end
+
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
